d69807b4a811f345c15f30eb2088b7eb7fadd631
iPrytz <isak.prytz@gmail.com>
2014-01-26 22:45:39 +0100

    products.java

--- Eclipse/ADI-floggit/src/se/adi/floggit/ui/Main.java
+++ Eclipse/ADI-floggit/src/se/adi/floggit/ui/Main.java
@@ -1,5 +1,6 @@
 package se.adi.floggit.ui;
 
+import se.adi.floggit.classes.Product;
 import se.adi.floggit.classes.User;
 
 public class Main
@@ -11,6 +12,12 @@ public class Main
 				"Fogdevagen 44", "12841", "Bagarmossen", "08-555 55 55"); 
 		
 		System.out.println(andrea.toString());
+		
+		Product test = new Product("En Sak", "En jattehaftig sak som gor saker", 999, 990, "Toys", "Garden", "Books");
+		
+		System.out.println(test.toString());
+		
+		
 	}
 
 }
