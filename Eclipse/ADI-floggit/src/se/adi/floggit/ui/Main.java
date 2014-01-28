ff7efa2709be352a2410d8208c40e996d025557f
iPrytz <isak.prytz@gmail.com>
2014-01-28 10:30:58 +0100

    empty main

--- Eclipse/ADI-floggit/src/se/adi/floggit/ui/Main.java
+++ Eclipse/ADI-floggit/src/se/adi/floggit/ui/Main.java
@@ -6,17 +6,31 @@ import se.adi.floggit.webshop.Webshop;
 
 public class Main {
 	public static void main(String[] args) {
-		Webshop webshop = new Webshop(new UserRepositoryInDB());
-		User isak = new User("dannie@hotmail.com", "secret", "Dannie", "Håkansson", "Plommonvägen 4", "263 65", "Viken");
+	
 		
-		if (webshop.updateUser("dannie@hotmail.com", isak)) {
-			System.out.println(isak.toString() + "was updated");
-		} else {
-			System.out.println("email was not found in the system!");
-		}
 		
-//		for (User u : webshop.readAllUsers()) {
-//			System.out.println(u);
-//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
